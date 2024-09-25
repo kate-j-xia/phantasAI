2eb746a46e54c3de3d6629817591a89aa3f4191c
Kate X <kate.j.xia@gmail.com>
2024-09-25 16:43:59 -0700

    updated image gen.py

--- backend/sd/gen.py
+++ backend/sd/gen.py
@@ -6,6 +6,7 @@ from PIL import Image
 import time
 
 model_id = "benjamin-paine/stable-diffusion-v1-5"
+# model_id = "stabilityai/stable-diffusion-3-medium-diffusers"
 
 def get_pipeline(use_image=False):
     start = time.time()
@@ -16,7 +17,7 @@ def get_pipeline(use_image=False):
         pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id, torch_dtype=float16)
     else:
         print(f'using text2Image pipeline...\n')
-        pipe = StableDiffusionPipeline.from_pretrained("benjamin-paine/stable-diffusion-v1-5")
+        pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=float16)
     pipe = pipe.to("cuda")  # Ensure it runs on GPU
     # pipe = pipe.to(torch.device("cuda")).half()
     pipe.enable_attention_slicing()
@@ -50,7 +51,7 @@ def from_image(pipe, prompt, init_image_path,
         return
     # Load the initial image
     start = time.time()
-    init_image = Image.open(init_image_path).convert("RGB")
+    init_image = Image.open(init_image_path)  # .convert("RGB")
     init_image = init_image.resize((768, 768))  # Resize to 512x512 if necessary
 
     # Generate image based on the uploaded picture
@@ -80,7 +81,9 @@ if __name__=="__main__":
     # prompt = "generate a cartoon cute version"
     # prompt = "an asian girl with big eyes and long hair in a dark, drapery, flowery dress"
     # prompt = "portrait photo of a old warrior chief"
-    prompt = "tintype photography of a melancholic gothic fairy-like female and flower, dark background"
+    # prompt = "tintype photography of a melancholic gothic fairy-like female and flower, dark background"
+    prompt = "a precise, black and white pencil sketch of a beautiful french 2-story farmhouse, " + \
+        "with a small pathway leading to the front door"
 
     input_image = "vv-frontyard conv.jpeg"
     
